e8d5fe9c9a0b9a24bc48ed911123a89fe548c4ef
Christopher Terrano <mterrano1@yahoo.com>
2022-01-05 14:51:55 -0600

    added mouseover event listener to buttons

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
 
-document.querySelector("body").appendChild(h2);
+function renderOneDrink(drink){
+    const grabDrinkInfo = document.querySelector('#drink-info');
+    const div = document.createElement('div');
+    div.id = drink.idDrink;
+    div.classList.add('drinkCard');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    p.textContent = drink.strDrink;
+    img.src = drink.strDrinkThumb;
+
+    grabDrinkInfo.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    img.width = 200;
+    img.height = 200;
+
+}
+
+function getDrinks(alcohol){
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
+    .then(res => res.json())
+    .then(data => {
+        let drinksList = data.drinks
+        drinksList.forEach(drink => renderOneDrink(drink))
+    })
+}
+
+document.querySelectorAll('.alcohol').forEach(btn => btn.addEventListener('mouseover', function(e){
+    e.target.style.cursor = 'pointer';
+}));
