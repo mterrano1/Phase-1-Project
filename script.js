bebb9f13fd76f43334208d9712e2efc82400281b
Christopher Terrano <mterrano1@yahoo.com>
2022-01-05 14:54:17 -0600

    created function to render drinks to page

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+function renderOneDrink(drink){
+    const grabDrinkInfo = document.querySelector('#drink-info');
+    const div = document.createElement('div');
+    div.id = drink.idDrink;
+    div.classList.add('drinkCard');
+    const img = document.createElement('img');
+    img.classList.add('drinkImages');
+    const p = document.createElement('p');
+    p.textContent = drink.strDrink;
+    img.src = drink.strDrinkThumb;
+
+    grabDrinkInfo.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    img.width = 200;
+    img.height = 200;
+
+}
+
 function getDrinks(alcohol){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
     .then(res => res.json())
