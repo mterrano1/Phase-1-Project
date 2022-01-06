6eb15d2dda63c2749d8b63e8c03a9904396f5110
Christopher Terrano <mterrano1@yahoo.com>
2022-01-05 19:26:23 -0600

    created function to clear page of drinks

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 
 function renderOneDrink(drink){
     const grabDrinkInfo = document.querySelector('#drink-info');
@@ -11,16 +9,22 @@ function renderOneDrink(drink){
     const p = document.createElement('p');
     p.textContent = drink.strDrink;
     img.src = drink.strDrinkThumb;
+    const btn = document.createElement('button');
+    btn.classList.add('likeBtns')
+    btn.textContent = 'Learn to make'
 
     grabDrinkInfo.appendChild(div);
     div.appendChild(img);
-    div.appendChild(p);
     img.width = 200;
     img.height = 200;
+    div.appendChild(p);
+    div.appendChild(btn);
 
 }
 
 function getDrinks(alcohol){
+    const grabDrinkInfo = document.querySelector('#drink-info');
+    clearPage(grabDrinkInfo);
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
     .then(res => res.json())
     .then(data => {
@@ -29,6 +33,17 @@ function getDrinks(alcohol){
     })
 }
 
-document.querySelectorAll('.alcohol').forEach(btn => btn.addEventListener('mouseover', function(e){
-    e.target.style.cursor = 'pointer';
+document.querySelectorAll('button').forEach(btn => btn.addEventListener('mouseover', function(e){
+    e.target.style.cursor = 'pointer'
 }));
+
+document.querySelectorAll('button').forEach(btn => btn.addEventListener('click', function(e){
+    let buttonText = e.target.innerText;
+    getDrinks(buttonText)
+}))
+
+function clearPage(parent){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
