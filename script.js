7bb1d4d2f0c93d8228dbb877d1b0f26413bbe621
Christopher Terrano <mterrano1@yahoo.com>
2022-01-05 23:43:10 -0600

    created event listener to fetch drink details

--- script.js
+++ script.js
@@ -20,6 +20,20 @@ function renderOneDrink(drink){
     div.appendChild(p);
     div.appendChild(btn);
 
+    btn.addEventListener('click', function(e){
+        let parentId = e.target.parentNode.id;
+        // clearPage(e.target.parentNode);
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${parentId}`)
+        .then(res => res.json())
+        .then(data => {
+            let drinksList = data.drinks;
+            const p = document.createElement('p');
+            for (key of drinksList)
+            document.getElementById(key.idDrink).appendChild(p)
+            p.textContent = key.strInstructions
+        })
+    })
+
     document.querySelectorAll('button').forEach(btn => btn.addEventListener('mouseover', function(e){
         e.target.style.cursor = 'pointer';
         e.target.style.opacity = 0.7;
@@ -30,6 +44,14 @@ function renderOneDrink(drink){
     }));
 
 }
+
+
+// function makeDrink(drink){
+//     const id = drink.idDrink;
+//     const p = document.createElement('p');
+//     p.textContent = drink.strDrink;
+    
+// }
 
 function getDrinks(alcohol){
     const grabDrinkInfo = document.querySelector('#drink-info');
